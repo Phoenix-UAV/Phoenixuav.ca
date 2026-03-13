350c1fab248c42dce6f1058a317378196b423ed6
ella-ti <marobinette@mun.ca>
2026-03-13 20:36:03 -0230

    added assets, fonts, etc

--- phoenixUAV/astro.config.mjs
+++ phoenixUAV/astro.config.mjs
@@ -1,9 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-     vite: {
-    plugins: [tailwindcss()],
+    vite: {
+    plugins: [tailwindcss()], 
   },
+  fonts: 
+    [
+      {
+      provider: fontProviders.local(),
+      name: "BankGothicBold",
+      cssVariable: "--bank-gothic-bold",
+      options: {
+        variants: [{
+          src: ['./src/assets/fonts/bankgothic-md-bt/BankGothicBold.ttf'],
+          weight: 'normal',
+          style: 'normal'
+         }]
+        }
+      } 
+    ]
 });
